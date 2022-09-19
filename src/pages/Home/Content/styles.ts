dd556897fa1e4702318a07555ac24895f3704ea3
Lucas Andrade <lucasdmandrade@gmail.com>
2022-09-18 22:20:34 -0300

    feat(Content) remove import not used

--- src/pages/Home/Content/styles.ts
+++ src/pages/Home/Content/styles.ts
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { isatty } from "tty";
 import { fontDefaultBold, fontDefaultBolder } from "../../../theme/text";
 
 export const Container = styled.div`
